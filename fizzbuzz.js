599a8bb91817e32c17205a561208ceba0775ad42
Elliot Metcalf <105979342+elliotmetcalf1@users.noreply.github.com>
2022-08-22 11:24:52 +0200

    Add files via upload
    basic fizz buzz exercise using if  and else if statements.

---
+++ fizzbuzz.js
@@ -0,0 +1,14 @@
+let num = 0
+
+if(num %15 === 0){
+    console.log('fizz buzz')
+}
+else if (num %5 === 0){
+    console.log('buzz')
+}
+else if (num %15 === 0){
+    console.log('fizz')
+}
+else{
+    console.log(num)
+}
